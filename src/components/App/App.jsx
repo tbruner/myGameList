1e120ea7451bf4fc74b7e4cc47a4ee5d4d31fd95
tbruner <trevor.a.bruner@gmail.com>
2023-06-11 19:59:18 -0400

    Add structure to homepage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GamePage from "./GamePage.jsx";
-import Homepage from "./Homepage.jsx";
-import Profile from "./Profile.jsx";
-import TitleBar from "./TitleBar/TitleBar.jsx";
+import GamePage from "../GamePage.jsx";
+import Homepage from "../Homepage.jsx";
+import Profile from "../Profile.jsx";
+import TitleBar from "../TitleBar/TitleBar.jsx";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,18 @@
 function Homepage() {
   return (
     <>
-      <h2>Home</h2>
+      <section className="home-hero">
+        <h1>Welcome to MyGameList</h1>
+        Explore new games and tackle your backlog
+        <ul>
+          <li>Catalog your game collection</li>
+          <li>Track and Tackle your gaming backlog</li>
+          <li>Share your Game List with friends</li>
+        </ul>
+      </section>
+      <section class="trending-games">
+        <h2>Trending Games</h2>
+      </section>
     </>
   );
 }
