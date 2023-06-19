829e39d34bf0b637105304631eb5aef64189c36d
tbruner <trevor.a.bruner@gmail.com>
2023-06-18 20:28:49 -0400

    Add structure to Profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,34 @@
+import "./Profile.css";
+
 function Profile() {
   return (
-    <>
-      <h2>Profile</h2>
-    </>
+    <div className="profile-page">
+      <img src="src/assets/cover-placeholder.png" alt="cover image"></img>
+
+      <div className="profile">
+        <div className="profile-card">
+          <img alt="user-avatar"></img>
+          <span className="user-screen-name">User Screen Name</span>
+          <div className="socials">
+            <div>Twitter</div>
+            <div>Twitch</div>
+            <div>YouTube</div>
+            <div>Facebook</div>
+          </div>
+        </div>
+        <div className="profile-content">
+          <div>
+            <span className="games-played">0</span> hrs of total playtime
+          </div>
+          <div>
+            <span className="games-played">0</span> completed
+          </div>
+          <div>
+            <span className="games-completed">0</span> in backlog
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
